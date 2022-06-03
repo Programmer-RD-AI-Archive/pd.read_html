d3ee7c68da53a5cdb2bae9ac7c4eb5d7b94be7f1
Programmer-RD-AI <go2ranuga@gmail.com>
2022-06-03 20:24:10 +0530

    Update download.py
    Co-Authored-By: Ranuga <79456372+Programmer-RD-AI@users.noreply.github.com>

--- download.py
+++ download.py
@@ -1,4 +1,8 @@
 import pandas as pd
 
-data = pd.read_html("https://en.wikipedia.org/wiki/List_of_massacres_in_the_United_States")
-data[0].to_csv("./data.csv", index=False)
+data = pd.read_html("https://en.wikipedia.org/wiki/List_of_best-selling_music_artists")
+n_data = data[0]
+for d_iter in data[1:]:
+    n_data = n_data.append(d_iter)
+print(n_data)
+n_data.to_csv("./data.csv", index=False)
